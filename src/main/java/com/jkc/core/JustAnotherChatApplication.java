f5ab94e082d0f598f285a34b30a37e72cc57fed7
Nishtha <nishthamittal4004@gmail.com>
2024-09-08 11:10:10 +0530

    intial setup

---
+++ src/main/java/com/jkc/core/JustAnotherChatApplication.java
@@ -0,0 +1,13 @@
+package com.jkc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAnotherChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAnotherChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkc/core/JustAnotherChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAnotherChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
